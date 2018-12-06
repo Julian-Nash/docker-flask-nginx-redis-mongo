7ce7a2d6727d47d360769c4ec75dc5095f8cff3e
Julian-Nash <venturidm@gmail.com>
2018-12-06 20:44:42 +0000

    removed mongo, changed nginx.conf to proxy_pass and changed location of nginx.conf

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
-from mongoengine import connect
 import redis
 
 app = Flask(__name__)
 
 r = redis.Redis(host='redis', port=6379, db=0)
-
-connect("flask-db", host="mongo")
 
 from app import views
 from app import models
--- flask/app/views.py
+++ flask/app/views.py
@@ -3,7 +3,6 @@ from app import r
 from app.models import Message
 
 from flask import render_template
-from mongoengine import NotUniqueError
 
 
 @app.route("/")
@@ -12,14 +11,4 @@ def index():
     r.incr("hits")
     hits = int(r.get("hits"))
 
-    try:
-        Message(
-            name="Julian Nash", 
-            message="And this message was saved and pulled from the database."
-        ).save()
-    except NotUniqueError:
-        pass
-
-    message = Message.objects().get()
-
-    return render_template("index.html", hits=hits, message=message)
+    return render_template("index.html", hits=hits)
