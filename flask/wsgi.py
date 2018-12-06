9486b5996078f41869a1a7d958bae230614cb092
Julian-Nash <venturidm@gmail.com>
2018-12-06 18:26:10 +0000

    update

--- flask/wsgi.py
+++ flask/wsgi.py
@@ -1,14 +1,6 @@
 from app import app
 from mongoengine import connect
 
-app.config.from_object('config.ProductionConfig')
-
-connect(
-    db=app.config["DB_NAME"],
-    username=app.config["DB_USERNAME"],
-    password=app.config["DB_PASSWORD"],
-    authentication_source=app.config["DB_AUTH_SOURCE"]
-    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
