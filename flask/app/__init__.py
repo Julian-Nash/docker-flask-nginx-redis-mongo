922e4f3ddb1ecb7e4ccc91a2205b42c5c4c605ee
Julian-Nash <venturidm@gmail.com>
2018-12-07 01:04:41 +0000

    added password auth to mongo and redis

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,11 +2,26 @@ from flask import Flask
 import redis
 from mongoengine import connect
 
+import os
+
 app = Flask(__name__)
 
-r = redis.Redis(host='redis', port=6379, db=0)
+r = redis.Redis(
+    host='redis', 
+    port=6379, 
+    db=0,
+    password="example"
+)
 
-connect(db="flask-db", host="mongo", port=27017)
+connect(
+    db="flask-db",
+    host="mongo",
+    port=27017,
+    username="root",
+    password="example",
+    authentication_source="admin"
+)
+
 
 from app import views
 from app import models
